722f7201cd8c419520a2196985e3fea42445941e
A. Utku Canbolat <50638609+utkucanbolat@users.noreply.github.com>
2022-11-17 09:12:18 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import clumpGenerator_Ferellec_McDowell
 import numpy as np
-from time import perf_counter
 
-t1 = perf_counter()
 
 CLUMP = clumpGenerator_Ferellec_McDowell.clumpGenerator_Ferellec_McDowell(
     stl_dir="Rock.stl",
@@ -12,6 +10,4 @@ CLUMP = clumpGenerator_Ferellec_McDowell.clumpGenerator_Ferellec_McDowell(
     pmax=1.0,
     output_dir='my_clump',
     isShuffled=True)
-
-print(perf_counter()-t1)
 
