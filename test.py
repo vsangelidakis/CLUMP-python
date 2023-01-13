3abd2a03bfffce588c1fcf22d0029bb8dc0466a5
utku <utkucanbolat@gmail.com>
2023-01-13 17:08:36 +0100

    Output param is added

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import clumpGenerator_Ferellec_McDowell
 import numpy as np
+from time import perf_counter
 
+t1 = perf_counter()
 
 CLUMP = clumpGenerator_Ferellec_McDowell.clumpGenerator_Ferellec_McDowell(
     stl_dir="Rock.stl",
@@ -10,4 +12,6 @@ CLUMP = clumpGenerator_Ferellec_McDowell.clumpGenerator_Ferellec_McDowell(
     pmax=1.0,
     output_dir='my_clump',
     isShuffled=True)
+
+print(perf_counter()-t1)
 
